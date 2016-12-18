03f632caf0d057f323f7fea355457b697754ad83
Euan Smith <euan1971@yahoo.co.uk>
2016-12-18 23:19:11 +0000

    reformatted to 2 space indentation

--- src/guid.js
+++ src/guid.js
@@ -7,71 +7,73 @@
  */
 
 
-var each=require('./objects').each;
+var each = require('./objects').each;
 
 function GUIDFactory(name) {
-    var guid = 1;
+  var guid = 1;
 
-    function ensureGUID(fn){
-        return fn[name] || (fn[name] = guid++);
+  function ensureGUID(fn) {
+    return fn[name] || (fn[name] = guid++);
+  }
+
+  function getGUID(fn) {
+    return fn && fn[name];
+  }
+
+  function add(store, obj) {
+    if (!store.hasOwnProperty('count')) {
+      Object.defineProperty(store, 'count', {value: 0, enumerable: false, writable: true});
     }
+    var objGUID = ensureGUID(obj);
+    if (!store.hasOwnProperty(objGUID))store.count++;
+    store[objGUID] = obj;
+    return obj;
+  }
 
-    function getGUID(fn){
-        return fn && fn[name];
+  function present(store, obj) {
+    return !!(obj[name] && store[ensureGUID(obj)]);
+  }
+
+  //NOTE: think about doing the store in an alternative way with a custom store iterator rather than assuming
+  //that it is a general object - with delete it won't perform well in V8, although that won't matter most
+  //of the time.  Sparse arrays (as the guids are integers) may be the way to go.
+  function remove(store, obj) {
+    var objGUID = getGUID(obj);
+    if (store.hasOwnProperty(objGUID)) {
+      store.count--;
+      return delete store[objGUID];
     }
+    return false;
+  }
 
-    function add(store, obj){
-        if (!store.hasOwnProperty('count')){
-            Object.defineProperty(store,'count',{value:0, enumerable: false, writable: true});
-        }
-        var objGUID = ensureGUID(obj);
-        if (!store.hasOwnProperty(objGUID))store.count++;
-        store[objGUID] = obj;
-        return obj;
-    }
-
-    function present(store, obj){
-        return !!(obj[name] && store[ensureGUID(obj)]);
-    }
-
-    //NOTE: think about doing the store in an alternative way with a custom store iterator rather than assuming
-    //that it is a general object - with delete it won't perform well in V8, although that won't matter most
-    //of the time.  Sparse arrays (as the guids are integers) may be the way to go.
-    function remove(store, obj){
-        var objGUID=getGUID(obj);
-        if (store.hasOwnProperty(objGUID)) {
-            store.count--;
-            return delete store[objGUID];
-        }
-        return false;
-    }
-
-    function addBinding(store, obj) {
-        add(store,obj);
-        return {
-            off: remove.bind(this, store, obj)
-        };
-    }
-
-    function callListeners(store, thisArg, args){
-        each(store,function(prop){prop.apply(thisArg, args)});
-    }
-
+  function addBinding(store, obj) {
+    add(store, obj);
     return {
-        ensureGUID:ensureGUID,
-        getGUID:getGUID,
-        hasGUID:getGUID,
-        add:add,
-        remove:remove,
-        addListener:addBinding,
-        removeListener:remove,
-        callListeners:callListeners,
-        present:present
+      off: remove.bind(this, store, obj)
     };
+  }
+
+  function callListeners(store, thisArg, args) {
+    each(store, function (prop) {
+      prop.apply(thisArg, args)
+    });
+  }
+
+  return {
+    ensureGUID: ensureGUID,
+    getGUID: getGUID,
+    hasGUID: getGUID,
+    add: add,
+    remove: remove,
+    addListener: addBinding,
+    removeListener: remove,
+    callListeners: callListeners,
+    present: present
+  };
 }
 
 /**
- * 
+ *
  * @type {{ensureGUID, getGUID, hasGUID, add, remove, addListener, removeListener, callListeners, present}}
  */
-module.exports=GUIDFactory('_$$lboGUID');
+module.exports = GUIDFactory('_$$lboGUID');
