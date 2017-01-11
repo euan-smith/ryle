bd6b74b959fa7f6dc3281ae31ba271d0ad22ec19
Euan Smith <euan1971@yahoo.co.uk>
2017-01-11 22:01:35 +0000

    refactored to a simpler state machine structure - much simpler to follow

--- examples/longRest/longRest.js
+++ examples/longRest/longRest.js
@@ -7,45 +7,51 @@ const $http=require('http-as-promised');
 
 //the base url of the rest service
 const url='http://localhost:3000';
-//ensure the response is parsed as json and prevent the 303 causing an auto redirect
-const opts={json:true, followRedirect:false};
 
+//ensure the response is parsed as json and prevent the 303 causing an auto redirect
+const jobOpts={json:true, followRedirect:false};
+
+//the final get for the result object should just be the body
+const resultOpts={resolve:'body'};
+
+//declare the state machine
 var getJob=ryle({
   get _start(){return this.postJob},
 
+  //POST to the list of jobs
   postJob(){
-    return ryle.on($http.post(url+'/jobs',opts),this.checkPost)
+    return ryle.on($http.post(url+'/jobs',jobOpts),this.checkJob)
   },
 
-  checkPost(context, result){
-    context.job=result[1];
-    if (result[0].statusCode!==202) {
-      throw new Error('Unexpected status code '+result[0].statusCode+' from POST');
-    }
-    if (!context.job.location) {
-      throw new Error('No location included in POST response');
-    }
-    return this.getJob;
-  },
-
-  getJob(context){
-    return ryle.on($http(url+context.job.location, opts), this.checkJob);
-  },
-
+  //check the result of either the POST or GET
   checkJob(context, result){
-    if (result[0].statusCode!==303) {
+    //store the result
+    context.job=result;
+
+    //if the response is a redirect then act on it and get the final result
+    if (result[0].statusCode===303) {
+      return this.getResult;
+    }
+
+    //otherwise if the body contains a property 'location' then get the job after a delay
+    if (result[1] && result[1].location) {
       return ryle.onTimeout(250,this.getJob);
     }
-    context.output=result[0].headers.location;
-    return this.getResult;
+
+    //otherwise the result was unexpected so throw an error
+    throw new Error('Unexpected response');
   },
 
+  //get the job object
+  getJob(context){
+    return ryle.on($http(url+context.job[1].location, jobOpts), this.checkJob);
+  },
+
+  //get the result
   getResult(context){
-    return ryle.on($http(url+context.output, opts), this.gotResult);
-  },
-
-  gotResult(context, result){
-    return ryle.exit(result[1]);
+    //because the options specify to return a promise which resolves to the body
+    //then this can just be returned as the result of the state machine
+    return ryle.exit($http(url+context.job[0].headers.location, resultOpts));
   }
 });
 
--- examples/longRest/longRestService.js
+++ examples/longRest/longRestService.js
@@ -13,14 +13,19 @@ var results=[];
 //POSTing to /jobs will return 202 (Accepted)
 app.post('/jobs',(req,res)=>{
   //how long the job will take - random time up to 2 seconds
-  tasks.push({completes: Date.now().valueOf()+Math.floor(Math.random()*2000)});
-  res.status(202).send({location:"/tasks/"+(tasks.length-1)});
+  var task={
+    completes: Date.now()+Math.floor(Math.random()*2000),
+    location:"/tasks/"+tasks.length,
+    status: "incomplete"
+  };
+  tasks.push(task);
+  res.status(202).send(task);
 });
 
 //GET a task in ptogress
 app.get('/tasks/:id',(req,res)=>{
   //check that the task exists
-  if (req.params.id<tasks.length && req.params.id>0){
+  if (req.params.id<tasks.length && req.params.id>=0){
     var task=tasks[req.params.id];
     //complete the task if it is due
     if (!task.result && task.completes<Date.now().valueOf()){
@@ -31,7 +36,7 @@ app.get('/tasks/:id',(req,res)=>{
     if (task.result){
       res.redirect(303, "/results/"+task.result);
     } else {
-      res.send({status: "incomplete"});
+      res.send(task);
     }
   } else {
     //the task does not exist
@@ -41,7 +46,7 @@ app.get('/tasks/:id',(req,res)=>{
 
 //return a result if it exists
 app.get('/results/:id',(req,res)=>{
-  if (req.params.id<results.length && req.params.id>0){
+  if (req.params.id<results.length && req.params.id>=0){
     res.send(results[req.params.id]);
   } else {
     res.sendStatus(404);
